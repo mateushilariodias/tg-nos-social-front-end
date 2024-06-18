a3e1ea0fc8d439b682da4901448f556278ae7c57
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-06-18 17:22:27 -0300

    builds homepage headwr buttons for desktop devices

--- client/src/components/homepage/header.tsx
+++ client/src/components/homepage/header.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
 
-function Header(){
+function Header() {
     return (
-        <header className="fixed z-10 w-full bg-slate-100 flex justify-between items-center py-5">
-            <Link className="font-bold text-sky-600 text-3xl" href='/homepage'>Nós Social</Link>    
+        <header className="fixed z-10 w-full bg-slate-100 flex justify-between items-center py-5 px-72">
+            <div>
+                <Link className="font-bold text-sky-600 lg:text-5xl" href="#">Nós Social</Link>
+            </div>
+                <div className="hidden lg:flex lg:gap-5 items-center font-bold text-white text-xl">
+                <Link href="/loginUser" className="bg-blue-600 hover:bg-blue-800 py-3 px-5 rounded-lg">
+                    <strong>Entrar como usuário</strong>
+                </Link>
+                <Link href="/registerUser" className="bg-blue-600 hover:bg-blue-800 py-3 px-5 rounded-lg">
+                    <strong>Cadastrar-se como usuário</strong>
+                </Link>
+            </div>
         </header>
     )
 }
