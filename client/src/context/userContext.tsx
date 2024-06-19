edf6ab0a168c5554fad2bb937386540589c404e2
mateushilariodias <mateusilariodias@gmail.com>
2024-06-19 13:32:45 -0300

    builds user context

---
+++ client/src/context/userContext.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { createContext, useEffect, useState } from "react";
+
+interface ContextProps {
+    children: React.ReactNode;
+}
+
+interface User {
+    user:
+    | {
+        id: number;
+        fullName: string,
+        userName: string,
+        emailUser: string,
+        phoneNumberUser: string,
+        passwordUser: string,
+        userImg: string,
+    } | undefined
+    setUser: (newState: any) => void;
+}
+
+const intialValue = {
+    user: undefined,
+    setUser: () => { },
+}
+
+export const UserContext = createContext<User>(intialValue)
+
+export const UserContextProvider = ({ children }: ContextProps) => {
+    const [user, setUser] = useState(intialValue.user);
+    useEffect(() => {
+        let UserJSON = localStorage.getItem("nos-social:user");
+        setUser(UserJSON && JSON.parse(UserJSON));
+    },[]);
+
+    return (
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
+            {children}
+        </UserContext.Provider >
+    )
+}
