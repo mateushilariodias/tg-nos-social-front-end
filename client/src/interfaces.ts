a9d75fd194551a025775af30020970d5eb018e82
mateushilariodias <mateusilariodias@gmail.com>
2024-06-19 13:20:05 -0300

    installs necessary dependencies and builds the interfaces for each project entity

---
+++ client/src/interfaces.ts
@@ -0,0 +1,51 @@
+export interface IPost {
+    id: number;
+    profilePicture: string;
+    author: string;
+    description: string;
+    image: string;
+    createdPost: string;
+    ngoId: number;
+}
+
+export interface ILike {
+    id: number;
+    userName: string;
+    likeUserId: number;
+    postId: number
+}
+
+export interface IComment {
+    id: number;
+    comment: number;
+    userName: string;
+    userImg: string;
+    commentUserId: number;
+    postId: number
+    createdComment: string;
+}
+
+export interface IUser {
+    id: number;
+    fullName: string,
+    userName: string,
+    emailUser: string,
+    phoneNumberUser: string,
+    passwordUser: string,
+    userImg: string,
+}
+
+export interface INgo {
+    id: number;
+    cnpj: string,
+    stateRegistration: string,
+    corporateReason: string,
+    emailNgo: string,
+    phoneNumberNgo: string,
+    physicalAddress: string,
+    objectiveOfTheNgo: string,
+    pageName: string,
+    imageNgo: string,
+    bgImageNgo: string,
+    registeringUser: string
+}
