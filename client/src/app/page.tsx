0aa26c20f453108471380315aa15b4699a39fdb7
mateushilariodias <mateusilariodias@gmail.com>
2024-06-19 21:11:23 -0300

    returns the user feed header

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/homepage/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Header/>
       <HeroSection />
     </main>
