6235d8a35464c30e073d68afc8a32eb1e4fb83e6
mateushilariodias <mateusilariodias@gmail.com>
2024-06-19 21:00:18 -0300

    build the hero section of the homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/homepage/header";
+import HeroSection from "@/components/homepage/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header/>
+      <HeroSection />
     </main>
   )
 }
