0dc1fafcea3e19ad56749164de5fee3316f5ba6a
Mateus Hilario Dias <mateus.dias@novahaus.com.br>
2024-06-25 14:55:59 -0300

    encode text quotes from the reset password page

--- client/src/app/(auth)/resetUserPassword/page.tsx
+++ client/src/app/(auth)/resetUserPassword/page.tsx
@@ -13,19 +13,19 @@ function ResetUserPassword() {
         <>
             <h1 className="font-bold text-2xl">Redefinir senha</h1>
             <h2 className="font-semibold text-lg">Etapa 1:</h2>
-            <span>Digite o e-mail que você cadastrou em nossa base de dados, em seguida clique em "Receber código por e-mail":</span>
+            <span>Digite o e-mail que você cadastrou em nossa base de dados, em seguida clique em &quot;Receber código por e-mail&quot;:</span>
             <AuthInput newState={setEmailUser} htmlForAndNameAndId="emailUser" label="E-mail:" type="email"></AuthInput>
             <Link href="/resetUserPassword" className="bg-blue-600 hover:bg-blue-800 py-3 font-bold text-white rounded-lg text-center">
                 <strong>Receber código por e-mail</strong>
             </Link>
             <h2 className="font-semibold text-lg">Etapa 2:</h2>
-            <span>Agora, insira o código recebido por e-mail e clique em "Validar código":</span>
+            <span>Agora, insira o código recebido por e-mail e clique em &quot;Validar código&quot;:</span>
             <AuthInput newState={setCode} htmlForAndNameAndId="code" label="Código:" type="text"></AuthInput>
             <Link href="/resetUserPassword" className="bg-blue-600 hover:bg-blue-800 py-3 font-bold text-white rounded-lg text-center">
                 <strong>Validar código</strong>
             </Link>
             <h2 className="font-semibold text-lg">Etapa 3:</h2>
-            <span>Pronto, você já pode digitar a sua nova senha e, logo apśs, clicar em "Registrar nova senha", para começar a usa-lá:</span>
+            <span>Pronto, você já pode digitar a sua nova senha e, logo apśs, clicar em &quot;Registrar nova senha&quot;, para começar a usa-lá:</span>
             <AuthInput newState={setPasswordUser} htmlForAndNameAndId="passwordUser" label="Nova senha:" type="password"></AuthInput>
             <Link href="/feedUser" className="bg-blue-600 hover:bg-blue-800 py-3 font-bold text-white rounded-lg text-center">
                 <strong>Registrar nova senha</strong>
